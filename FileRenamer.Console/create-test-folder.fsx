9ebba3e410b6d53bbd76257c5240a72b2f762ca0
Johannes Egger <eggerjohansi@gmail.com>
2015-12-18 21:51:18 +0100

    Add script to create test folder

---
+++ FileRenamer.Console/create-test-folder.fsx
@@ -0,0 +1,18 @@
+﻿open System.IO
+
+let dir = @"C:\Users\Hannes\Workspace\wklaufen\file-rename\test"
+let indexFilePath = Path.Combine(dir, "index.txt")
+
+try
+    Directory.Delete(dir, true)
+with _ -> ()
+
+Directory.CreateDirectory dir |> ignore
+[1..10]
+|> List.map (sprintf "File %d.pdf")
+|> List.map (fun f -> Path.Combine(dir, f))
+|> List.iter (fun p -> File.WriteAllText(p, sprintf "Content of file %s" p))
+
+[5..13]
+|> List.map (sprintf "Blub %d")
+|> fun l -> File.WriteAllLines(indexFilePath, l)
