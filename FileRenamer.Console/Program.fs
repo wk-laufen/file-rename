f01a25c574eb2d8bd838d985121315b85a372112
Johannes Egger <eggerjohansi@gmail.com>
2015-12-20 23:06:23 +0100

    Search index file in app dir. Reference .NET 3.5. Catch exceptions

--- FileRenamer.Console/Program.fs
+++ FileRenamer.Console/Program.fs
@@ -3,45 +3,63 @@
 open System
 open System.IO
 
+module Array =
+    let tryItem idx (list: 'a array) =
+        if idx >= 0 && idx < list.Length
+        then Some list.[idx]
+        else None
+
 let getArg key =
     let args = Environment.GetCommandLineArgs()
     args
     |> Seq.tryFindIndex (fun a -> a.Equals("-" + key, StringComparison.InvariantCultureIgnoreCase))
     |> Option.bind (fun idx -> args |> Array.tryItem (idx + 1))
 
-let rename() =
-    getArg "dir"
-    |> Choice.ofOption "Missing command line arg \"dir\""
-    |> Choice.bind (fun dir ->
-        Directory.Exists dir
-        |> Choice.ofBool (sprintf "Directory %s doesn't exist" dir)
-        |> Choice.map (fun () -> dir)
-    )
-    |> Choice.bind (fun dir ->
-        let filePattern = "*.txt"
-        Directory.GetFiles(dir, filePattern)
-        |> Seq.tryHead
-        |> Choice.ofOption (sprintf "Index file (%s) not found in %s" filePattern dir);
-        |> Choice.bind (fun indexFile ->
-            FileRenamer.Core.BatchRename.renameFiles dir indexFile (fun source target -> ())
-        )
-        |> Choice.map (fun _ -> dir)
-    )
+let assemblyDir =
+    System.Reflection.Assembly.GetExecutingAssembly().Location
+    |> Path.GetDirectoryName
 
-let log =
+let renameImpl dir indexFile =
+    FileRenamer.Core.BatchRename.renameFiles dir indexFile (fun source target -> ())
+    |> Choice.map (fun _ -> dir)
+
+let rename() =
+    let dir =
+        getArg "dir"
+        |> Choice.ofOption "Missing command line arg \"dir\""
+        |> Choice.bind (fun dir ->
+            Directory.Exists dir
+            |> Choice.ofBool (sprintf "Directory %s doesn't exist" dir)
+            |> Choice.map (fun () -> dir)
+        )
+
+    let indexFile =
+        let fileName = "index.txt"
+        let path = Path.Combine(assemblyDir, fileName)
+        if File.Exists path
+        then Choice1Of2 path
+        else Choice2Of2 (sprintf "Couldn't find index file %s in %s." fileName assemblyDir)
+
+    let (<*>) = Choice.apply
+    (Choice1Of2 renameImpl) <*> dir <*> indexFile
+    |> Choice.bind id
+
+let log format =
     let continuation s =
-        let dir =
-            System.Reflection.Assembly.GetExecutingAssembly().Location
-            |> Path.GetDirectoryName
-        File.AppendAllLines(Path.Combine(dir, "log.txt"), [|s|])
-    Printf.ksprintf continuation
+        File.AppendAllText(Path.Combine(assemblyDir, "log.txt"), s + Environment.NewLine)
+    Printf.ksprintf continuation format
 
 [<EntryPoint>]
 let main argv =
-    match rename() with
-    | Choice1Of2 dir ->
-        log "Successfully renamed files in %s" dir
-        0
-    | Choice2Of2 error ->
-        log "Error while renaming files: %s" error
-        -1
+    try
+        match rename() with
+        | Choice1Of2 dir ->
+            log "Successfully renamed files in %s" dir
+            0
+        | Choice2Of2 error ->
+            log "Error while renaming files: %s" error
+            -1
+    with e ->
+        eprintfn "%O" e
+        log "%O" e
+        reraise()
--- FileRenamer.Core/BatchRename.fs
+++ FileRenamer.Core/BatchRename.fs
@@ -17,18 +17,19 @@ let private deleteFile path =
         File.Delete path |> Choice1Of2
     with e -> Choice2Of2 e.Message
 
+let private (@@) a b =
+    Path.Combine(a, b)
+
 let renameFiles dir indexFile renameProgress =
     let allLines = File.ReadAllLines indexFile
     let allFiles = Directory.GetFiles(dir, "*.pdf")
 
     let lines =
-        allLines
-        |> Array.take (Math.Min(allLines.Length, allFiles.Length))
+        allLines.[0..Math.Min(allLines.Length - 1, allFiles.Length - 1)]
         |> Array.toList
 
     let files =
-        allFiles
-        |> Array.take (Math.Min(allLines.Length, allFiles.Length))
+        allFiles.[0..Math.Min(allLines.Length - 1, allFiles.Length - 1)]
         |> Array.toList
 
     let tmpDir = Path.Combine(dir, "tmp")
@@ -44,7 +45,7 @@ let renameFiles dir indexFile renameProgress =
         Directory.GetFiles tmpDir
         |> List.ofArray
         |> List.map (fun f ->
-            let newFilePath = Path.Combine(Path.GetDirectoryName f, "..", Path.GetFileName f)
+            let newFilePath = (Path.GetDirectoryName f) @@ ".." @@ (Path.GetFileName f)
             deleteFile newFilePath
             |> Choice.bind (fun () -> moveFile f newFilePath)
             |> Choice.map (fun () -> Path.GetFileName newFilePath)
